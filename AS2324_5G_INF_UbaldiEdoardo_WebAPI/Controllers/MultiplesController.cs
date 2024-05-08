e23b27dfaecef2713949b55e5176ef37dd0c8e72
Edoardo Ubaldi <Edoubba@gmail.com>
2024-05-08 08:17:53 +0200

    add MultiplesController

---
+++ AS2324_5G_INF_UbaldiEdoardo_WebAPI/Controllers/MultiplesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_UbaldiEdoardo_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MultiplesController : ControllerBase
+    {
+        [HttpGet("Multiples")]
+        public JsonResult Multiples(int num, int factor)
+        {
+            return new JsonResult(
+                new
+                {
+                    res = (num % factor) == 0,
+                });
+        }
+    }
+}
