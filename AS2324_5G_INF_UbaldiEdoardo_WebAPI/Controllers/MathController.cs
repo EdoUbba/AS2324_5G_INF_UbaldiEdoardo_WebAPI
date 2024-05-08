283ef76e244a7cfd4962d347d63f8a1ba05475b7
Edoardo Ubaldi <Edoubba@gmail.com>
2024-05-08 08:47:30 +0200

    add potenza() + change name MultiplesController to MathController

---
+++ AS2324_5G_INF_UbaldiEdoardo_WebAPI/Controllers/MathController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS2324_5G_INF_UbaldiEdoardo_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        string status_result = "KO";
+
+        [HttpGet("multiplointero")]
+        public JsonResult multiplointero(int num, int factor)
+        {
+            
+            return new JsonResult(
+                new
+                {
+                    status_result,
+                    res = (num % factor) == 0,
+                });
+        }
+
+        [HttpGet("potenza")]
+        public JsonResult potenza(int b, int esponente)
+        {
+            int res = 1;
+            for(int i = 0; i < esponente; i++)
+            {
+                res *= b;
+            }
+
+            return new JsonResult(
+                new
+                {
+                    status_result = "OK",
+                    res
+                });
+        }
+
+        
+    }
+}
